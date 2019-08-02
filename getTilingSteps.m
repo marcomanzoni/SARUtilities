b175d5124b3e0332a8ed025e8685d24510db08be
Marco Manzoni <markoarko@gmail.com>
2019-08-02 18:51:15 +0200

    The function that allows tiling is done

---
+++ getTilingSteps.m
@@ -0,0 +1,42 @@
+function [readMatrix, writeMatrix, singleTileSize, numberOfTiles] = getTilingSteps(Naz, tileSize, windowSize)
+%GETTILINGSTEPS Generate the matrices with the indexes where I should start
+%to read and write a stack in the case of a processing carried out using
+%tiles.
+%   Inputs: 
+%           - Nr: number of azimuth lines. Usually the tiling happens in
+%           the azimuth, meaning that I take all the range lines from n to
+%           n+tileSize, so I segment along the azimuth
+%           - tileSize: size of a single tile. Keep in mind that the last
+%           one can be smaller
+%           - Overhead at the beginning of each tile
+%
+%   Outputs:
+%           - readMatrix: [numberOfTiles x 2] matrix. At each step of
+%           the tiling process start reading at readingMatrix(ii,1) and
+%           stop at readingMatrix(ii,2). At the same time start writing at
+%           writeMatrix(ii,1) and stop at writeMatrix(ii,2). Remember to
+%           remove from the dataset the first and last (windowsSize-1)/2
+%           samples
+%           - singleTileSize: size of the single tile that I'm reading,
+%           this is useful for the last tile that can be smaller
+%           - numberOfTiles: scalar containing the total number of tiles
+%           that I'm creating
+
+
+startReadVector     = 1:tileSize-1-(windowSize-1)/2:Naz;
+stopReadVector      = startReadVector+tileSize-1;
+stopReadVector(end) = Naz;
+
+readMatrix          = [startReadVector(:), stopReadVector(:)];
+singleTileSize      = readMatrix(:,2)-readMatrix(:,1)+1;
+
+numberOfTiles       = length(singleTileSize);
+
+startWriteVector    = readMatrix(:,1) + (windowSize-1)/2;
+stopWriteVector     = readMatrix(:,2) - (windowSize-1)/2;
+
+writeMatrix         = [startWriteVector(:), stopWriteVector(:)];
+
+
+end
+
