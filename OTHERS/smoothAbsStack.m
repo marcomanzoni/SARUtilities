c67c311b35ae3aac98b75429f6d60643831cfa53
Marco Manzoni <markoarko@gmail.com>
2019-09-16 16:11:36 +0200

    Added function to smooth the stack in amplitude

---
+++ OTHERS/smoothAbsStack.m
@@ -0,0 +1,27 @@
+function image = smoothAbsStack(image)
+%SMOOTHABSSTACK Smoothing of a SAR image (amplitude) based on the amplitude
+%over median value.
+%
+% inputs:
+%           image: [Nr, Na, Ni double complex] stack of images in input
+% outputs:
+%           image: [Nr, Na, Ni double complex] stack of images in output
+[Nr, Na, Ni] = size(image);
+% apply subsampling to scan stats
+subsampling = ceil(Nr*Na / 5e4);
+% cut samples that are ovr_med above median
+overMedianFactor = 4;
+
+for jj=1:Ni
+    tmp = image(:,:,jj);
+    amplitudeTemp = abs(tmp);
+    amplitudeTempSub = amplitudeTemp(1:subsampling:end);
+    sca = overMedianFactor * median(amplitudeTempSub(amplitudeTempSub>0), 'omitnan' );
+    ind_pk = amplitudeTemp(:) > sca;
+    if any(ind_pk)
+        tmp(ind_pk) = exp(1i*angle(tmp(ind_pk))).*sca;
+    end
+    image(:,:,jj) = tmp;
+end
+end
+
